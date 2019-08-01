57ece528207e60ba9ae133b1140023affb04c8b9
Ken Ryan <chen97me@gmail.com>
2019-08-01 09:18:17 -0700

    Some progress on email. There is a bug in the email_creating.py file

--- email/email_creating.py
+++ email/email_creating.py
@@ -1,9 +1,9 @@
-from jinja2 import Template
 import sys
+from jinja2 import Template
 
 campaign_id = sys.argv[1]
-
-rendered = Template(open('first_day/email.html').read()).render(heatmap = "https://jinja.palletsprojects.com", time = '20', areas = "Santa Monica");
+###Some sort of bug going on here
+rendered = Template(open('first_day/email.html').read()).render(heatmap = "https://jinja.palletsprojects.com", time = 'asd', areas = "Santa Monica");
 
 text_file = open("ready_email/"+campaign_id+"email","w")
 text_file.write(rendered)

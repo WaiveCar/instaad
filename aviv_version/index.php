493b8fbe8ea07d8a1200a0197e05d55bc5189022
Chris McKenzie <kristopolous@yahoo.com>
2019-08-05 19:31:52 -0700

    working on a blur background for the image

--- aviv_version/index.php
+++ aviv_version/index.php
@@ -22,7 +22,7 @@ function check($x) {
 }
 $handle = trim($_GET['user']);
 $contents = file_get_contents("https://instagram.com/$handle");
-$bigtext = $_GET['bigtext'] ?: "@$handle";
+$bigtext = $_GET['bigtext'] ?: "<small>@</small>$handle";
 $smalltext = $_GET['smalltext'] ?: json_decode('"' . get('full_name') . '"');
 $logo = get('profile_pic_url_hd');
 
@@ -46,6 +46,21 @@ $loop = $_GET['loop'] ? 'infinite' : '';
   animation: <?= $dur ?>s <?= $loop ?>;
   animation-fill-mode: forwards;
 }
+#blur {
+  position: absolute;
+  top: 0; 
+  left: 0;
+  height: 35.1vw;
+  overflow: hidden;
+}
+#blur img {
+  margin-top: 2vw;
+  margin-left: -27.5%;
+  height: 70.2vw;
+  opacity: 0.05;
+  border-radius: 500vw;
+  
+}
 body.dark { 
   color: #fff;
   background: #000 
@@ -59,17 +74,25 @@ h2 {
   font-weight: 400;
   margin-top: 2vw;
 }
+small {
+  display: inline-block;
+  font-size: 75%;
+  padding-bottom: 0.15em;
+  margin-right: 0.1em;
+  vertical-align: bottom;
+}
 h3 {
   font-size: 1.75vw;
   font-weight: 300;
 }
 #wrap {
   height: 35.1vw;
+  box-shadow: 0 0 0 4px #000;
   position: relative;
   overflow: hidden;
 }
 #wrap > div {
-  width: calc(100%/3);
+  width: 35.1vw;
   text-align: center;
 }
 .row {
@@ -81,16 +104,18 @@ h3 {
 }
 .row img {
   object-fit: cover;
-  max-height: calc(100vw/3 - 2vw);
+  max-height: calc(35.1vw + 1vw);
+  min-height: calc(35.1vw);
 }
 img.fill {
-  min-width: calc(100vw/3 - 2vw);
+  min-width: calc(35.1vw + 1vw);
 }
-#brand {
+#wrap > #brand {
   display: flex;
   align-items: center;
   justify-content: space-around;
   height: 35.1vw;
+  width: calc(100% - 2 * 34.6vw);
   transition-timing-function: ease;
   animation-name: logo;
 }
@@ -106,14 +131,12 @@ img.fill {
 }
 #brand img {
   border-radius: 50vw;
-  border: 1px solid #e4e4e4;
-  padding: 0.2vw;
+  box-shadow: 0 0.2vw 0.8vw 0vw rgba(0,0,0,0.125);
   width: 100%;
   animation-name: zoomout;
 }
 .row.down {
   bottom: 0;
-  margin-bottom: -0.2vw;
   animation-name: slidedown;
 }
 .row.up {
@@ -124,16 +147,16 @@ img.fill {
   display: flex;
   align-items: center;
   justify-content: space-around;
-  /*box-shadow: 0 0 0 4px #fff;*/
+  box-shadow: 0 0.1vw 0.4vw 0vw rgba(0,0,0,0.25);
   border-radius: 2vw;
-  height: calc(100vw/3 - 2vw);
-  width: calc(100vw/3 - 2vw);
-  margin: 2vw 0;
+  height: calc(35.1vw - 2vw);
+  width: calc(35.1vw - 2vw);
+  margin: 1vw 0;
   overflow: hidden
 }
 @keyframes zoomout {
   0% { margin-top: -35vw;}
-  5% { margin-top: 0 }
+  2% { margin-top: 0 }
   90% { width: 100%; opacity: 1 }
   97%,100% { width: 0; opacity: 0 }
 }
@@ -144,25 +167,28 @@ img.fill {
 @keyframes logo {
   0% { margin-left: 33%; opacity: 0 }
   5% { opacity: 1 }
-  10%,77% { margin-left: 0 }
+  10%,77% { margin-left: .5vw }
   90%,100% { margin-left: 33% }
 }
 @keyframes slideup {
-  0% { transform: translateY(35.33vw); opacity: 0 }
+  0% { transform: translateY(36.1vw); opacity: 0 }
   10%,23% { transform: translateY(0); opacity: 1 }
-  36%,50% { transform: translateY(-35.33vw) }
-  63%,77% { transform: translateY(calc(-35.33vw * 2)); opacity: 1 }
-  90%,100% { transform: translateY(calc(-35.33vw * 3)); opacity: 0 }
+  36%,50% { transform: translateY(-35.1vw) }
+  63%,77% { transform: translateY(calc(-35.1vw * 2)); opacity: 1 }
+  90%,100% { transform: translateY(calc(-35.1vw * 3)); opacity: 0 }
 }
 @keyframes slidedown {
-  from { transform: translateY(-35.33vw); opacity: 0 }
+  from { transform: translateY(-35.1vw); opacity: 0 }
   10%,23% { transform: translateY(0); opacity: 1 }
-  36%,50% { transform: translateY(35.33vw) }
-  63%,77% { transform: translateY(calc(35.33vw * 2)); opacity: 1 }
-  90%,100% { transform: translateY(calc(35.33vw * 3)); opacity: 0 }
+  36%,50% { transform: translateY(35.1vw) }
+  63%,77% { transform: translateY(calc(35.1vw * 2)); opacity: 1 }
+  90%,100% { transform: translateY(calc(35.1vw * 3)); opacity: 0 }
 }
 </style>
-<body class=dark>
+<body>
+  <div id=blur>
+    <img src=<?= $logo ?>>
+  </div>
   <div id=wrap>
     <div id=brand>
       <div>
@@ -175,12 +201,12 @@ img.fill {
         </div>
       </div>
     </div>
-    <div class='row up' style=left:33.3%>
+    <div class='row up' style='left:calc(100% - 34.6vw * 2)'>
     <? foreach([0,1,2] as $i){ ?>
     <div><img <?=check($i);?>src=<?= $images[$i] ?>></div>
     <? } ?>
     </div>
-    <div class='row down' style=left:66.6%>
+    <div class='row down' style='left:calc(100% - 35.1vw)'>
     <? foreach([5,4,3] as $i){ ?>
     <div><img <?=check($i);?>src=<?= $images[$i] ?>></div>
     <? } ?>

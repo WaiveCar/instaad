f28ed49623391fc295a0b6e34966e0c81fbebaf7
Ken Ryan <chen97me@gmail.com>
2019-08-09 20:16:07 -0700

    Bug fix

---
+++ website/website/main/routes.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template, request
+
+main = Blueprint('main', __name__)
+
+@main.route("/")
+@main.route("/home")
+def home():
+    return (render_template('index.html',title='WaiveAd'))
+
+@main.route("/callback")
+    return redirect(url_for('main.home'))
